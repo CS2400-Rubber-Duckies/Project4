856ac11a582cc2dd0e702ea32c7788f6a99a63ed
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2022-04-19 13:33:16 -0700

    main

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args){
+    try{
+        ArrayList<Integer> readArray = new ArrayList<Integer>() ;
+        File file = new File("/Users/gelilachaka/downloads/data_random.txt");
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextInt()){
+        readArray.add(reader.nextInt());
+        }
 
+
+
+
+    }
+
+
+
+catch(IOException e){
+    System.out.println("Error while reading") ;
+    }
+}}
